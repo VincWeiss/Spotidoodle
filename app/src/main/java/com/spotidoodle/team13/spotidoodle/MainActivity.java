500a987b59778bbe963a771d381ab78b7b662bb6
OxanaDoroshkevich <oxana.doroshkevich@student.reutlingen-university.de>
2017-06-01 10:39:26 +0200

    added the start screen design in android and the all around working implementation of with the sha1 fingerprint of vinc account

--- app/src/main/java/com/spotidoodle/team13/spotidoodle/MainActivity.java
+++ app/src/main/java/com/spotidoodle/team13/spotidoodle/MainActivity.java
@@ -2,9 +2,11 @@ package com.spotidoodle.team13.spotidoodle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
 import android.util.Log;
+import android.view.Window;
+import android.widget.Button;
+import android.view.View;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -19,107 +21,142 @@ import com.spotify.sdk.android.player.SpotifyPlayer;
 
 public class MainActivity extends AppCompatActivity implements
         SpotifyPlayer.NotificationCallback, ConnectionStateCallback
-        {
-            // TODO: Replace with your client ID
-            private static final String CLIENT_ID = "227c5a2e60684041932779eeaf4176e1";
-            // TODO: Replace with your redirect URI
-            private static final String REDIRECT_URI = "Spotidoodle-login://callback";
+{
+    // TODO: Replace with your client ID
+    //SHA1 Fingerprint 3D:5B:E8:6A:56:D1:FF:EC:91:AB:A8:27:50:13:A1:A6:85:35:CA:F6
+    private static final String CLIENT_ID = "9f703a39b15a4241b08dcea6685e5f50";
+    // TODO: Replace with your redirect URI
+    private static final String REDIRECT_URI = "http://spotidoodle2.com/callback/";
 
-            private Player mPlayer;
+    private Player mPlayer;
 
-            // Request code that will be used to verify if the result comes from correct activity
-// Can be any integer
-            private static final int REQUEST_CODE = 1337;
+    // Request code that will be used to verify if the result comes from correct activity
+    // Can be any integer
+    private static final int REQUEST_CODE = 1337;
 
-            @Override
-            protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-                AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
-                        AuthenticationResponse.Type.TOKEN,
-                        REDIRECT_URI);
-                builder.setScopes(new String[]{"user-read-private", "streaming"});
-                AuthenticationRequest request = builder.build();
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
 
-                AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+        Button playlistOne = (Button) findViewById(R.id.button);
+        playlistOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
             }
+        });
+    }
 
-            @Override
-            protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-                super.onActivityResult(requestCode, resultCode, intent);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
 
-                // Check if result comes from the correct activity
-                if (requestCode == REQUEST_CODE) {
-                    AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
-                    if (response.getType() == AuthenticationResponse.Type.TOKEN) {
-                        Config playerConfig = new Config(this, response.getAccessToken(), CLIENT_ID);
-                        Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
-                            @Override
-                            public void onInitialized(SpotifyPlayer spotifyPlayer) {
-                                mPlayer = spotifyPlayer;
-                                mPlayer.addConnectionStateCallback(MainActivity.this);
-                                mPlayer.addNotificationCallback(MainActivity.this);
-                            }
-
-                            @Override
-                            public void onError(Throwable throwable) {
-                                Log.e("MainActivity", "Could not initialize player: " + throwable.getMessage());
-                            }
-                        });
-                    }
-                }
-            }
-
-            @Override
-            protected void onDestroy() {
-                Spotify.destroyPlayer(this);
-                super.onDestroy();
-            }
-
-            @Override
-            public void onPlaybackEvent(PlayerEvent playerEvent) {
-                Log.d("MainActivity", "Playback event received: " + playerEvent.name());
-                switch (playerEvent) {
-                    // Handle event type as necessary
-                    default:
-                        break;
-                }
-            }
-
-            @Override
-            public void onPlaybackError(Error error) {
-                Log.d("MainActivity", "Playback error received: " + error.name());
-                switch (error) {
-                    // Handle error type as necessary
-                    default:
-                        break;
-                }
-            }
-
-            @Override
-            public void onLoggedIn() {
-                Log.d("MainActivity", "User logged in");
-
-                mPlayer.playUri(null, "spotify:track:4aSon118zcGeJoKINAmiav", 0, 0);
-            }
-            @Override
-            public void onLoggedOut() {
-                Log.d("MainActivity", "User logged out");
-            }
-
-            @Override
-            public void onLoginFailed(Error e) {
-                Log.d("MainActivity", "Login failed");
-            }
-
-            @Override
-            public void onTemporaryError() {
-                Log.d("MainActivity", "Temporary error occurred");
-            }
-
-            @Override
-            public void onConnectionMessage(String message) {
-                Log.d("MainActivity", "Received connection message: " + message);
+        // Check if result comes from the correct activity
+        if (requestCode == REQUEST_CODE) {
+            AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
+            switch (response.getType()) {
+                // Response was successful and contains auth token
+                case TOKEN:
+                    onAuthenticationComplete(response);
+                    break;
+                // Auth flow returned an error
+                case ERROR:
+                    Log.d("Auth error: ", response.getError());
+                    break;
+                // Most likely auth flow was cancelled
+                default:
+                    Log.d("Auth error: ", response.getType().toString());
             }
         }
+    }
+
+    private void onAuthenticationComplete(AuthenticationResponse authResponse) {
+        // Once we have obtained an authorization token, we can proceed with creating a Player.
+        Log.d("myTag", "Got authentication token");
+        if (mPlayer == null) {
+            Config playerConfig = new Config(getApplicationContext(), authResponse.getAccessToken(), CLIENT_ID);
+            // Since the Player is a static singleton owned by the Spotify class, we pass "this" as
+            // the second argument in order to refcount it properly. Note that the method
+            // Spotify.destroyPlayer() also takes an Object argument, which must be the same as the
+            // one passed in here. If you pass different instances to Spotify.getPlayer() and
+            // Spotify.destroyPlayer(), that will definitely result in resource leaks.
+            mPlayer = Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
+                @Override
+                public void onInitialized(SpotifyPlayer player) {
+                    Log.d("myTag", "-- Player initialized --");
+                    //mPlayer.setConnectivityStatus(mOperationCallback, getNetworkConnectivity(MainActivity.this));
+                    mPlayer.addNotificationCallback(MainActivity.this);
+                    mPlayer.addConnectionStateCallback(MainActivity.this);
+                    // Trigger UI refresh
+                    //updateView();
+                }
+
+                @Override
+                public void onError(Throwable error) {
+                    error.printStackTrace();
+                }
+            });
+        } else {
+            mPlayer.login(authResponse.getAccessToken());
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        Spotify.destroyPlayer(this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onPlaybackEvent(PlayerEvent playerEvent) {
+        Log.d("MainActivity", "Playback event received: " + playerEvent.name());
+        switch (playerEvent) {
+            // Handle event type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onPlaybackError(Error error) {
+        Log.d("MainActivity", "Playback error received: " + error.name());
+        switch (error) {
+            // Handle error type as necessary
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onLoggedIn() {
+        Log.d("MainActivity", "User logged in");
+
+        mPlayer.playUri(null, "spotify:track:4aSon118zcGeJoKINAmiav", 0, 0);
+    }
+    @Override
+    public void onLoggedOut() {
+        Log.d("MainActivity", "User logged out");
+    }
+
+    @Override
+    public void onLoginFailed(Error e) {
+        Log.d("MainActivity", "Login failed");
+    }
+
+    @Override
+    public void onTemporaryError() {
+        Log.d("MainActivity", "Temporary error occurred");
+    }
+
+    @Override
+    public void onConnectionMessage(String message) {
+        Log.d("MainActivity", "Received connection message: " + message);
+    }
+}
---
+++ app/src/main/java/com/spotidoodle/team13/spotidoodle/SortMusicActivity.java
@@ -0,0 +1,10 @@
+package com.spotidoodle.team13.spotidoodle;
+
+/**
+ * Created by Oxana on 01.06.2017.
+ */
+
+public class SortMusicActivity {
+
+
+}
