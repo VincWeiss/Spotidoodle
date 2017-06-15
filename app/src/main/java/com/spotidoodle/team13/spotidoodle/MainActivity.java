1fa1aa1f94124699cda5092ba9107ca3f4294ea0
OxanaDoroshkevich <oxana.doroshkevich@student.reutlingen-university.de>
2017-06-15 13:52:40 +0200

    added the web api to the project and get the connection

--- app/src/main/java/com/spotidoodle/team13/spotidoodle/MainActivity.java
+++ app/src/main/java/com/spotidoodle/team13/spotidoodle/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.Window;
 import android.widget.Button;
 import android.view.View;
+import android.widget.TextView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
 import com.spotify.sdk.android.authentication.AuthenticationRequest;
@@ -24,13 +25,21 @@ import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
 import java.io.Serializable;
+import java.util.List;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyCallback;
+import kaaes.spotify.webapi.android.SpotifyError;
 import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.UserPrivate;
+import kaaes.spotify.webapi.android.models.UserPublic;
+import retrofit.Callback;
 import retrofit.RequestInterceptor;
 import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
-public class MainActivity extends AppCompatActivity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback, Player {
+public class MainActivity extends AppCompatActivity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
 
     //SHA1 Fingerprint 3D:5B:E8:6A:56:D1:FF:EC:91:AB:A8:27:50:13:A1:A6:85:35:CA:F6
     private static final String CLIENT_ID = "9f703a39b15a4241b08dcea6685e5f50";
@@ -50,9 +59,22 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
         }
     };
 
-    // Request code that will be used to verify if the result comes from correct activity
-    // Can be any integer
+    private final SpotifyCallback spotifyCallback = new SpotifyCallback() {
+        @Override
+        public void failure(SpotifyError spotifyError) {
+            System.out.println("ERROR:" + spotifyError);
+        }
+
+        @Override
+        public void success(Object o, Response response) {
+            System.out.println("OK!");
+        }
+    };
+
     private static final int REQUEST_CODE = 1337;
+    private List myPlaylists;
+    private UserPrivate user;
+    private SpotifyApi api;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,6 +87,7 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
         builder.setScopes(new String[]{"user-read-private", "streaming"});
         AuthenticationRequest request = builder.build();
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+
 
         Button playlistOne = (Button) findViewById(R.id.button);
         Button playlisttTwo = (Button) findViewById(R.id.button2);
@@ -100,60 +123,41 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         super.onActivityResult(requestCode, resultCode, intent);
-        // Check if result comes from the correct activity
         if (requestCode == REQUEST_CODE) {
             AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, intent);
             switch (response.getType()) {
-                // Response was successful and contains auth token
                 case TOKEN:
                     onAuthenticationComplete(response);
                     break;
-                // Auth flow returned an error
                 case ERROR:
                     Log.d("Auth error: ", response.getError());
                     break;
-                // Most likely auth flow was cancelled
                 default:
                     Log.d("Auth error: ", response.getType().toString());
             }
         }
     }
 
-    private void onButtonClicked() {
-        final Button playlistOne = (Button) findViewById(R.id.button);
-        playlistOne.setOnClickListener(new View.OnClickListener() {
+    private void onAuthenticationComplete(AuthenticationResponse authResponse) {
+        this.api = new SpotifyApi();
+        this.api.setAccessToken(authResponse.getAccessToken());
+        final TextView userName = (TextView) findViewById(R.id.textView);
+        SpotifyService spotify = api.getService();
+        spotify.getMe(new Callback<UserPrivate>() {
             @Override
-            public void onClick(View v) {
-                //mPlayer.playUri(mOperationCallback, TEST_PLAYLIST_URI, 0, 0);
+            public void success(UserPrivate userPrivate, Response response) {
+                user = userPrivate;
+                userName.setText(user.display_name.toString());
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                userName.setText("Max Mustermann");
+                System.out.println(error.getStackTrace());
             }
         });
-    }
-
-    private void getSpotifyWebAPI() {
-        final String accessToken = "myAccessToken";
-
-        RestAdapter restAdapter = new RestAdapter.Builder()
-                .setEndpoint(SpotifyApi.SPOTIFY_WEB_API_ENDPOINT)
-                .setRequestInterceptor(new RequestInterceptor() {
-                    @Override
-                    public void intercept(RequestFacade request) {
-                        request.addHeader("Authorization", "Bearer " + accessToken);
-                    }
-                })
-                .build();
-
-        SpotifyService spotify = restAdapter.create(SpotifyService.class);
-    }
-
-    private void onAuthenticationComplete(AuthenticationResponse authResponse) {
-        // Once we have obtained an authorization token, we can proceed with creating a Player.
         if (mPlayer == null) {
             Config playerConfig = new Config(getApplicationContext(), authResponse.getAccessToken(), CLIENT_ID);
-            // Since the Player is a static singleton owned by the Spotify class, we pass "this" as
-            // the second argument in order to refcount it properly. Note that the method
-            // Spotify.destroyPlayer() also takes an Object argument, which must be the same as the
-            // one passed in here. If you pass different instances to Spotify.getPlayer() and
-            // Spotify.destroyPlayer(), that will definitely result in resource leaks.
             mPlayer = Spotify.getPlayer(playerConfig, this, new SpotifyPlayer.InitializationObserver() {
                 @Override
                 public void onInitialized(SpotifyPlayer spotifyPlayer) {
@@ -233,115 +237,5 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     @Override
     public void onConnectionMessage(String message) {
         Log.d("MainActivity", "Received connection message: " + message);
-    }
-
-    @Override
-    public boolean addNotificationCallback(NotificationCallback notificationCallback) {
-        return false;
-    }
-
-    @Override
-    public boolean removeNotificationCallback(NotificationCallback notificationCallback) {
-        return false;
-    }
-
-    @Override
-    public void initialize(Config config) {
-
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-
-    @Override
-    public boolean login(String s) {
-        return false;
-    }
-
-    @Override
-    public boolean logout() {
-        return false;
-    }
-
-    @Override
-    public boolean addConnectionStateCallback(ConnectionStateCallback connectionStateCallback) {
-        return false;
-    }
-
-    @Override
-    public boolean removeConnectionStateCallback(ConnectionStateCallback connectionStateCallback) {
-        return false;
-    }
-
-    @Override
-    public void playUri(OperationCallback operationCallback, String s, int i, int i1) {
-
-    }
-
-    @Override
-    public void queue(OperationCallback operationCallback, String s) {
-
-    }
-
-    @Override
-    public void pause(OperationCallback operationCallback) {
-
-    }
-
-    @Override
-    public void resume(OperationCallback operationCallback) {
-
-    }
-
-    @Override
-    public void skipToNext(OperationCallback operationCallback) {
-
-    }
-
-    @Override
-    public void skipToPrevious(OperationCallback operationCallback) {
-
-    }
-
-    @Override
-    public void seekToPosition(OperationCallback operationCallback, int i) {
-
-    }
-
-    @Override
-    public void setShuffle(OperationCallback operationCallback, boolean b) {
-
-    }
-
-    @Override
-    public void setRepeat(OperationCallback operationCallback, boolean b) {
-
-    }
-
-    @Override
-    public void setPlaybackBitrate(OperationCallback operationCallback, PlaybackBitrate playbackBitrate) {
-
-    }
-
-    @Override
-    public void setConnectivityStatus(OperationCallback operationCallback, Connectivity connectivity) {
-
-    }
-
-    @Override
-    public void refreshCache() {
-
-    }
-
-    @Override
-    public Metadata getMetadata() {
-        return null;
-    }
-
-    @Override
-    public PlaybackState getPlaybackState() {
-        return null;
     }
 }
